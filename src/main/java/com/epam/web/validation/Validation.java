30018a384b50a71d44d35f8f640b6c014d6493cc
mockturtleqt <galia.semashko@gmail.com>
2016-11-30 21:59:43 +0300

    Initial commit

---
+++ src/main/java/com/epam/web/validation/Validation.java
@@ -0,0 +1,34 @@
+package com.epam.web.validation;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.IOException;
+
+public class Validation {
+    private static final Logger logger = LogManager.getLogger();
+
+
+    public static boolean validateXMLSchema(String xsdPath, String xmlPath) {
+        boolean isValid;
+        try {
+            SchemaFactory factory =
+                    SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = factory.newSchema(new File(xsdPath));
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(new File(xmlPath)));
+            isValid = true;
+        } catch (IOException | SAXException e) {
+            logger.error(e);
+            isValid = false;
+        }
+        return isValid;
+    }
+}
